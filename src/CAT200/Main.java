56d41dc541728aa8f7bf3c896529937db5945629
zhenrong1999 <zhenrong1999@student.usm.my>
2019-10-20 13:56:13 +0800

    Setting Up Main Window Already

--- src/CAT200/Main.java
+++ src/CAT200/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 430));
         primaryStage.show();
     }
 
--- src/CAT200/MainWindowController.java
+++ src/CAT200/MainWindowController.java
@@ -1,4 +1,39 @@
 package CAT200;
 
-public class MainWindowController {
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MainWindowController implements Initializable {
+    @FXML
+    private Button Home,Add,Search,Exit;
+    @FXML
+    private Label Title;
+    @FXML
+    private void handlebuttonaction(ActionEvent event){
+        if(event.getSource()==Home)
+            Title.setText("Home");
+        else if(event.getSource()==Add)
+            Title.setText("Add Student");
+        else if (event.getSource()==Search)
+            Title.setText("Search Student");
+        else if(event.getSource()==Exit)
+            Title.setText("Exit");
+            // get a handle to the stage
+            Stage stage = (Stage) Exit.getScene().getWindow();
+            stage.close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
